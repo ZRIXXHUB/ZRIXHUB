4bf16ebc61443406b0d305ee547f8339ecf7a05e
Zrix x Hub <92664687+ZRIXXHUB@users.noreply.github.com>
2023-01-07 18:26:56 +0700

    Update ROYALETHAIARMY.lua

--- ROYALETHAIARMY.lua
+++ ROYALETHAIARMY.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("TITLE", "DarkTheme")
+local Window = Library.CreateLib("ZRIX HUB", "DarkTheme")
 local Tab = Window:NewTab("Player")
 local Section = Tab:NewSection("Section Name")
 
